6e566ad0e0fcbe155473474968182e94917d9b6f
mahnoorkhalid8 <mahnoorkhalid814@gmail.com>
2026-01-23 00:22:00 +0500

    bot

--- Phase-3/frontend/app/dashboard/page.tsx
+++ Phase-3/frontend/app/dashboard/page.tsx
@@ -607,7 +607,6 @@ export default function DashboardPage() {
                       </div>
                     </div>
                   )}
-                  )}
                 </li>
               ))}
             </ul>
